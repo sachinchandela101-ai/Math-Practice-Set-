a0b82bb64e14c750f66ec0b74569f2787d8e1e96
sachinchandela101-ai <sachinchandela101@gmail.com>
2025-09-19 21:11:19 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -32,7 +32,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     _bannerAd = BannerAd(
-      adUnitId: "ca-app-pub-8223246456088610~4153228652", // <-- यहां अपनी Banner Ad Unit ID डालो
+      adUnitId: "ca-app-pub-8223246456088610/3971118029, // <-- यहां अपनी Banner Ad Unit ID डालो
       request: AdRequest(),
       size: AdSize.banner,
       listener: BannerAdListener(
